2909f8c3e4888c6d6417894034287a4743a47d16
Narianapereira <nariana.pereira@outlook.com>
2023-04-08 12:59:35 -0300

    IMPLEMENTING AUTHENTICATION

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-with-spring-boot-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package br.com.erudio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.model.User;
+import io.swagger.v3.oas.annotations.Parameter;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	@Query("SELECT u FROM User WHERE u.userName =:userName")
+	User findByUserName(@Param("userName") String userName);
+	
+	
+}
